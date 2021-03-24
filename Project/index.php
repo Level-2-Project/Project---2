338336a24f3b875ec6665887d9f2f488b7ade434
RadoslavConev <rts.conev@gmail.com>
2021-03-24 17:15:27 +0200

    24.03.2021

--- Project/index.php
+++ Project/index.php
@@ -1,6 +1,8 @@
 <?php 
 include '../Project/includes/header.php';
 
+session_start();
+
 // $filter_query = '';
 
 // if(isset($_GET['filter'])){
@@ -23,7 +25,8 @@ $result = mysqli_query( $conn, $read_query );
 if( mysqli_num_rows( $result ) > 0 ){
 
 	?>
-	<h1>Facebook Posts<span><a href="create.php" class="btn btn-info">Create Post</a></span></h1>
+	<h1>Facebook Posts<span><a href="post.php" class="btn btn-info <?=(!isset($_SESSION['user_id'])) 
+		? 'disabled' : ''  ?>" >Create Post</a></span></h1>
 
 	<form action="index.php" method="get" accept-charset="utf-8">
 		<input type="text" name="filter" value='' placeholder="Enter Keyword">
